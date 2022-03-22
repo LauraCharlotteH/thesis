0b415a0ce631c66dcff6b32c033e257e569cbbf4
Laura Utz <lauracharlotte.hofmann@stud.tu-darmstadt.de>
2022-03-22 17:05:52 +0100

    amazon first popup styling

--- src/app/pages/amazon/amazon.page.ts
+++ src/app/pages/amazon/amazon.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {PopUpComponent} from './pop-up/pop-up.component';
+import {ModalController} from '@ionic/angular';
 
 @Component({
   selector: 'app-amazon',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AmazonPage implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) { }
 
-  ngOnInit() {
-  }
+   async ngOnInit() {
+     const modal = await this.modalCtrl.create({
+       component: PopUpComponent,
+       backdropDismiss: false,
+       initialBreakpoint: 0.4,
+       componentProps: {
+         name: 'cluster is XXX'
+       },
+       cssClass: 'amazon-modal'
+     });
+
+     modal.onDidDismiss().then(async (data: any) => {
+       if (data.data) {
+         console.log('data is: ' + data.data);
+       }
+     });
+     return await modal.present();
+
+   }
 
 }
