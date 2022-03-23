9ac513b42138f76c77c12a373f0f30234a95cd1c
Laura Utz <lauracharlotte.hofmann@stud.tu-darmstadt.de>
2022-03-23 15:12:31 +0100

    amazon second popup designed (and selection functionality)

--- src/app/pages/amazon/adjust/adjust.component.ts
+++ src/app/pages/amazon/adjust/adjust.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ScoreCardService} from '../../../services/score-card/score-card.service';
 import {ModalController} from '@ionic/angular';
 import {Router} from '@angular/router';
-import {ICookieScore} from '../../../interfaces/interfaces';
+import {ActiveStatus, ICookieScore} from '../../../interfaces/interfaces';
 
 @Component({
   selector: 'app-adjust',
@@ -10,6 +10,11 @@ import {ICookieScore} from '../../../interfaces/interfaces';
   styleUrls: ['./adjust.component.scss'],
 })
 export class AdjustComponent implements OnInit {
+  selectedCookies: {[key: string]: ActiveStatus} = {
+    adds: ActiveStatus.neutral,
+    amazonAdds: ActiveStatus.neutral,
+    thirdPartyAdds : ActiveStatus.neutral,
+  };
 
   constructor(public scoreCardService: ScoreCardService, protected modalCtrl: ModalController, private router: Router) { }
 
@@ -39,6 +44,38 @@ export class AdjustComponent implements OnInit {
     console.log('confirming!');
     this.modalCtrl.dismiss();
     this.router.navigateByUrl('/wikipedia');
+  }
+
+  addsChange(event) {
+    if(event.detail.value === 'on'){
+      this.selectedCookies.adds = ActiveStatus.act;
+      this.selectedCookies.amazonAdds = ActiveStatus.act;
+      this.selectedCookies.thirdPartyAdds = ActiveStatus.act;
+    }else{
+      this.selectedCookies.adds = ActiveStatus.notact;
+      this.selectedCookies.amazonAdds = ActiveStatus.notact;
+      this.selectedCookies.thirdPartyAdds = ActiveStatus.notact;
+    }
+  }
+
+  amazonAddsChange(event){
+    if(event.detail.value === 'on'){
+      this.selectedCookies.adds = (this.selectedCookies.thirdPartyAdds === ActiveStatus.act)?ActiveStatus.act: ActiveStatus.neutral;
+      this.selectedCookies.amazonAdds = ActiveStatus.act;
+    }else{
+      this.selectedCookies.adds = (this.selectedCookies.thirdPartyAdds === ActiveStatus.notact)?ActiveStatus.notact: ActiveStatus.neutral;
+      this.selectedCookies.amazonAdds = ActiveStatus.notact;
+    }
+  }
+
+  thirdPartyAddsChange(event){
+    if(event.detail.value === 'on'){
+      this.selectedCookies.adds = (this.selectedCookies.amazonAdds === ActiveStatus.act)?ActiveStatus.act: ActiveStatus.neutral;
+      this.selectedCookies.thirdPartyAdds = ActiveStatus.act;
+    }else{
+      this.selectedCookies.adds = (this.selectedCookies.amazonAdds === ActiveStatus.notact)?ActiveStatus.notact: ActiveStatus.neutral;
+      this.selectedCookies.thirdPartyAdds = ActiveStatus.notact;
+    }
   }
 
 }
